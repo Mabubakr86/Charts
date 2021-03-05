e451aef539da368f8a18d4df885d73a8c74b08dd
Mabubakr86 <dev.mabubakr@gmail.com>
2021-03-05 08:57:29 +0200

    adding from vs

--- charts.py
+++ charts.py
@@ -1,34 +1,41 @@
 import os
 import pandas as pd
+import matplotlib.pyplot as plt
 
-path_to_chart_data = r''
+path_to_charts_data = r'F:\scripts\general\charts'
+os.chdir(path_to_charts_data)
+data = {'1/8':'chart1.csv'}
 
-os.chdir(path_to_chart_data)
-
-def get_y(path_to_chart_data,x_limit = 0.5, y_limit = 0.35 ,x_value = -1 ):
+def get_y(x,ser=None):
     """
     Return y value float
     Get Value from Chart , 
     Value of x
     x_limit limit of x axis
     """
-    df= pd.read_excel(path_to_chart_data)
+    df= pd.read_csv(data[ser])
     plot = plt.plot(df['x'],df['y'])
     xvalues = plot[0].get_xdata()
     yvalues = plot[0].get_ydata()
     x_limit = xvalues[-1]
+    xmin = xvalues[0]
+    y_limit = yvalues[-1]
+    ymin = yvalues[0]
     plt.close()
 
     y = None
-    if 0 < x_value < x_limit: 
+    if xmin < x < x_limit: 
         for ind, val in enumerate (xvalues):
-            if x_value > xvalues[ind-1] :
-                if x_value < val:
+            if x > xvalues[ind-1] :
+                if x < val:
                     y = yvalues[ind-1]
 
-    elif x_value < 0 :
-        y = 0
-    elif x_value > x_limit :
+    elif x < xmin :
+        y = ymin
+    elif x > x_limit :
         y = y_limit
 
     return y
+
+if __name__ == '__main__':
+    print(get_y(x=99.6, ser='1/8'))
